df9371ae6f2e6cc18a71538a02ac46db5b705aa5
Dániel Buga <bugadani@gmail.com>
2025-11-25 22:22:25 +0100

    Update generator to handle newly generated yamls better

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, path::Path, time::Instant};
+use std::{
+    collections::{HashMap, HashSet},
+    path::Path,
+    time::Instant,
+};
 
 use probe_rs_target::{ChipFamily, MemoryAccess, MemoryRegion, NvmRegion, RamRegion};
 use stm32_data_serde::{
@@ -11,7 +15,7 @@ use stm32_data_serde::{
 use target_gen::commands::elf::serialize_to_yaml_string;
 
 fn main() {
-    let embassy_data = load_stm_data("sources/stm32-data-generated/data/chips/");
+    let mut embassy_data = load_stm_data("sources/stm32-data-generated/data/chips/");
 
     //for (family, chips) in &embassy_data {
     //    println!("  {family}: {}", chips.len());
@@ -21,67 +25,111 @@ fn main() {
 
     let mut unknown_variants = Vec::new();
     let start = Instant::now();
-    for (family_name, chips) in embassy_data.iter() {
+    for (family_name, embassy_chips) in embassy_data.iter_mut() {
         let probe_rs_data = format!("sources/probe-rs/probe-rs/targets/{family_name}_Series.yaml");
         let output = format!("output/{family_name}_Series.yaml");
-
-        println!("Processing {family_name}");
 
         if !Path::new(&probe_rs_data).exists() {
             println!("Skipping {family_name} as probe-rs data does not exist");
             continue;
         }
 
+        println!("Processing {family_name}");
+
         let yaml = std::fs::read_to_string(&probe_rs_data).unwrap();
         let mut family_data = serde_yaml::from_str::<ChipFamily>(&yaml).unwrap();
 
-        for device in chips.iter() {
-            let mut memories = device.memory[0].clone(); // TODO: support multi-bank?
+        preprocess_family(&mut family_data, embassy_chips);
+
+        for embassy_device in embassy_chips.iter() {
+            let mut memories = embassy_device.memory[0].clone(); // TODO: support multi-bank?
             memories.sort_by(|a, b| a.address.cmp(&b.address));
 
-            update_variant(&mut family_data, &device.name, &device.name, &memories);
+            update_variant(
+                &mut family_data,
+                &embassy_device.name,
+                &embassy_device.name,
+                &memories,
+            );
 
             add_package_variants(
                 &mut family_data,
-                device
+                &embassy_device.name,
+                embassy_device
                     .packages
                     .iter()
-                    .map(|variant| (device.name.as_str(), variant.name.clone())),
+                    .map(|variant| variant.name.clone()),
             );
         }
 
         deduplicate_package_variants(&mut family_data);
-        deduplicate_by_name(&mut family_data);
 
-        unknown_variants.extend(extra_variants(&family_data, &chips));
+        unknown_variants.extend(remove_unknown_variants(&mut family_data, &embassy_chips));
 
         let yaml = serialize_to_yaml_string(&family_data).unwrap();
         std::fs::write(&output, yaml)
             .unwrap_or_else(|e| panic!("Failed to write to {output}: {e}"));
     }
 
-    for variant in unknown_variants {
-        println!("embassy data is missing {variant}");
+    if !unknown_variants.is_empty() {
+        println!("The following chip variants were not found in embassy-stm32 data and have been deleted from probe-rs:");
+        for variant in unknown_variants {
+            println!(" - {variant}");
+        }
     }
 
     let end = start.elapsed();
     println!("Finished in {:.02}s", end.as_secs_f32());
 }
 
-fn extra_variants<'a>(
-    family_data: &'a ChipFamily,
-    chips: &'a [Chip],
-) -> impl Iterator<Item = String> + 'a {
+fn preprocess_family(family_data: &mut ChipFamily, embassy_chips: &mut Vec<Chip>) {
+    // Normalize chip names. probe-rs data may contain one-variant-per-chip data when freshly
+    // generated from a pack, or many-variants-per-chip data when updating already processed data.
+    // This function essentially erases variant data that hasn't been processed by this tool.
+    for chip in family_data.variants.iter_mut() {
+        // We don't care about packaging.
+        let chip_name = chip.name.trim_end_matches("TR");
+
+        // Find the embassy chip where the chip or variant name equals chip_name:
+        let Some(embassy_chip) = embassy_chip_from_variant(embassy_chips, chip_name) else {
+            println!("embassy data is missing {chip_name}");
+            continue;
+        };
+        chip.name = embassy_chip.name.clone();
+    }
+
+    // Deduplicate probe-rs chips
+    let mut seen = HashSet::new();
     family_data
-        .variants()
+        .variants
+        .retain(|chip| seen.insert(chip.name.clone()));
+
+    // Don't bother processing embassy chips that don't have probe-rs data. These need to be added via a CMSIS-Pack update.
+    embassy_chips.retain(|chip| family_data.variants.iter().any(|v| v.name == chip.name));
+}
+
+fn embassy_chip_from_variant<'a>(embassy_chips: &'a [Chip], chip_name: &str) -> Option<&'a Chip> {
+    embassy_chips
         .iter()
-        .flat_map(|chip| chip.package_variants())
-        .filter(|chip| {
-            !chips
-                .iter()
-                .any(|c| &c.name == *chip || c.packages.iter().any(|p| &p.name == *chip))
-        })
-        .map(|chip| chip.clone())
+        .find(|c| &c.name == chip_name || c.packages.iter().any(|p| p.name == chip_name))
+}
+
+fn remove_unknown_variants(family_data: &mut ChipFamily, chips: &[Chip]) -> Vec<String> {
+    let mut extras = vec![];
+
+    for chip in family_data.variants.iter_mut() {
+        chip.package_variants.retain(|chip| {
+            let exists = embassy_chip_from_variant(chips, chip).is_some();
+
+            if !exists {
+                extras.push(chip.clone());
+            }
+
+            exists
+        });
+    }
+
+    extras
 }
 
 fn load_stm_data(arg: &str) -> HashMap<String, Vec<Chip>> {
@@ -127,25 +175,21 @@ fn deduplicate_package_variants(family_data: &mut ChipFamily) {
     }
 }
 
-fn add_package_variants<'a>(
+fn add_package_variants(
     family_data: &mut ChipFamily,
-    chip_variants: impl Iterator<Item = (&'a str, String)>,
+    device: &str,
+    chip_variants: impl Iterator<Item = String>,
 ) {
-    for (device, package) in chip_variants {
+    for package in chip_variants {
         // Look up device in family
         let Some(variant) = family_data.variants.iter_mut().find(|v| v.name == device) else {
-            println!("Missing from probe-rs: {package}");
+            println!("Missing from probe-rs: {device}");
             continue;
         };
 
         // Add package variant
         variant.package_variants.push(package);
     }
-}
-
-fn deduplicate_by_name(family_data: &mut ChipFamily) {
-    let mut seen = std::collections::HashSet::new();
-    family_data.variants.retain(|v| seen.insert(v.name.clone()));
 }
 
 fn update_variant(
